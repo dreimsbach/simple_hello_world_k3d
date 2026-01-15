cad53b0c5ece0cdaaade2477ddbf5143e47fd90d
Dirk Reimsbach <dreimsbach@medienwerft.de>
2026-01-15 10:49:13 +0100

    Update 2026

--- bin/create_cluster.sh
+++ bin/create_cluster.sh
@@ -9,15 +9,25 @@ mkdir -p $localVolumePath/hello-world
 echo "<html><head></head><body><h1>Local HTML File Heelo World<h1></body></html>" > $localVolumePath/hello-world/index.html
 
 # Create k3d Cluster with NGINX as Ingress and mount local folder als Volume
-k3d cluster create hello-world-test \
+#k3d cluster create hello-world-test \
+#  --port 8089:8089@loadbalancer  \
+#  --port 80:80@loadbalancer  \
+#  --port 443:443@loadbalancer  \
+# --servers 1  \
+#  --volume "$localVolumePath:/usr/share/k3dvolume/" \
+#  --volume "$(pwd)/base/helm/helm-ingress-nginx.yaml:/var/lib/rancher/k3s/server/manifests/helm-ingress-nginx.yaml" \
+#  --k3s-server-arg '--no-deploy=traefik' \
+#  --servers-memory=2g
+
+k3d cluster create hello-word-test \
   --port 8089:8089@loadbalancer  \
   --port 80:80@loadbalancer  \
   --port 443:443@loadbalancer  \
   --servers 1  \
   --volume "$localVolumePath:/usr/share/k3dvolume/" \
   --volume "$(pwd)/base/helm/helm-ingress-nginx.yaml:/var/lib/rancher/k3s/server/manifests/helm-ingress-nginx.yaml" \
-  --k3s-server-arg '--no-deploy=traefik' \
-  --servers-memory=2g
+  --k3s-arg "--disable=traefik@server:*" \
+  --servers-memory=2g \
 
 #Kustomize apply
 kubectl apply -k .
--- bin/delete_cluster.sh
+++ bin/delete_cluster.sh
@@ -1,1 +1,1 @@
-k3d cluster delete hello-world-test
+k3d cluster delete hello-word-test
