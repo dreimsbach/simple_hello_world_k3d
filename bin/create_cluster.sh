61d7b976bf9b22755c3316086654700038ad0ec0
Dirk Reimsbach <dreimsbach@medienwerft.de>
2026-01-20 18:42:47 +0100

    Paperless funktioniert

--- bin/create_cluster.sh
+++ bin/create_cluster.sh
@@ -6,7 +6,10 @@ localVolumePath="$repo_root/k3dvolume"
 
 # Create Folder if not exists
 mkdir -p "$localVolumePath/hello-world"
-mkdir -p "$localVolumePath/paperless-ngx"
+mkdir -p "$localVolumePath/paperless/paperless-ngx"
+mkdir -p "$localVolumePath/paperless/paperless-ngx-media"
+mkdir -p "$localVolumePath/paperless/paperless-ngx-consume"
+mkdir -p "$localVolumePath/paperless/paperless-ngx-postgres"
 
 # add index html file local
 echo "<html><head></head><body><h1>Local HTML File Hello World</h1></body></html>" > "$localVolumePath/hello-world/index.html"
