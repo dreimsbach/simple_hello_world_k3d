b1d3882b41aef104f7bde631af1140c7e1513720
Dirk Reimsbach <dreimsbach@medienwerft.de>
2026-02-09 12:33:56 +0100

    Update

--- bin/create_cluster.sh
+++ bin/create_cluster.sh
@@ -1,9 +1,10 @@
 
 #Local Base Folder from GIT resources
-localVolumePath=$(pwd)/../k3dvolume
+localVolumePath=$(pwd)/k3dvolume
 
 # Create Folder if not exists
 mkdir -p $localVolumePath/hello-world
+mkdir -p $localVolumePath/rancher
 
 # add index html file local
 echo "<html><head></head><body><h1>Local HTML File Hello World<h1></body></html>" > $localVolumePath/hello-world/index.html
